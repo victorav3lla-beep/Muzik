98b33dd9633eaac6a714617f5b918fd5b491730e
Victor Avella <victorhugo.avellalemus@gmail.com>
2025-11-25 12:49:26 +0100

    added devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :playlists, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :chats, dependent: :destroy
