f0465dab6283ce002786e4d31212e591566c6705
jOHN wiNTERS <JOHNWINTERS91@GMAIL.COM>
2025-11-25 17:04:14 +0100

    create a playlist option

---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,30 @@
+class PlaylistsController < ApplicationController
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = current_user.Playlist.new(playlist_params)
+    # @playlist.made = RubyLLM.chat.ask("Generate a playlist from this text: #{@playlist.queary}").content
+    if @playlist.save
+    redirect_to playlist_path(@playlist), notice: "Playlist was successfully created! WOOOOO!!🎉🎉"
+    else
+    render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @playlists = current_user.playlists
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
+  end
+
+  private
+
+  def playlist_params
+    params.require(playlist:).permit(:title, :content)
+  end
+
+end
