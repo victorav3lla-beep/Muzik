daa7c6da0d4a9e4dd964f4dd41bbadd6d65f4ebe
Victor Avella <victorhugo.avellalemus@gmail.com>
2025-11-26 15:05:36 +0100

    adding prompts

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,6 @@
 class ChatsController < ApplicationController
+
+SYSTEM_PROMPT = "You are a Playlists Creator\n\nI am a Spotify user who wants to create playlists using AI.\n\nHelp me create playlists, doing questions to understand what I want from it.\n\nAnswer with a playlist related to the prompt."
   def show
     @chat    = current_user.chats.find(params[:id])
     @message = Message.new
@@ -12,15 +14,19 @@ class ChatsController < ApplicationController
     @chat.user = current_user
 
     if @chat.save
+      ruby_llm_chat = RubyLLM.chat
+      response = ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
+      Message.create
+
       redirect_to chat_path(@chat)
     else
-      render "playlists/show"
+      render "playlists/show", status: :unprocessable_entity
     end
   end
 
   private
 
   def chat_params
-    params.require(:chat).permit(:title,)
+    params.require(:chat).permit(:title)
   end
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,4 +2,10 @@ class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :playlist
   has_many :messages, dependent: :destroy
+
+  DEFAULT_TITLE = "Untitled"
+  TITLE_PROMPT = <<~PROMPT
+  Generate a short, descriptive, 3-to-6-word title that summarizes the user question for a chat conversation.
+  PROMPT
+
 end
